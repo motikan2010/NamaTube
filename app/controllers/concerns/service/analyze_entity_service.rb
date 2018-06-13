c0fdf2af3321dedc131a6a530aef069fc6c4c02e
KouheiAgena <motikan2010@yahoo.co.jp>
2018-06-14 00:46:34 +0900

    add 構文解析のサービスを追加

---
+++ app/controllers/concerns/service/analyze_entity_service.rb
@@ -0,0 +1,16 @@
+require 'httpclient'
+
+module Service::AnalyzeEntityService
+  extend ActiveSupport::Concern
+
+  def analyze_sentence(sentence)
+    request_content = {:document => {:type => 'PLAIN_TEXT', :content => "#{sentence}"}, :encodingType => 'UTF8'}
+    client = HTTPClient.new
+    endpoint_url = "https://language.googleapis.com/v1/documents:analyzeEntities?key=#{ENV['GOOGLE_API_KEY']}"
+    # client.debug_dev=STDOUT # ログ出力
+    res = client.post_content(endpoint_url.to_s, request_content.to_json, 'Content-Type' => 'application/json')
+
+    JSON.parse(res)
+  end
+
+end
